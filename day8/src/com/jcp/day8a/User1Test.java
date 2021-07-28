ea42dc752e078e2fdaa4933b75c40e432605c08e
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-07-28 11:12:31 +0900

    day8 - (1)

---
+++ day8/src/com/jcp/day8a/User1Test.java
@@ -0,0 +1,15 @@
+package com.jcp.day8a;
+
+public class User1Test {
+
+	public static void main(String[] args) {
+		
+		
+		User1 momo = new User1();
+		
+		momo.name ="모모";
+		momo.age = 25;
+
+	}
+
+}

2223f5a92742749f4ebec4d273e83e24c98dde65
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-08-02 11:57:37 +0900

    day 11 -오전2

--- day11/src/day11b/SmartTVTest.java
+++ day11/src/day11b/SmartTVTest.java
@@ -1,12 +1,12 @@
 package day11b;
 
 public class SmartTVTest {
-
 	public static void main(String[] args) {
 		// 2개의 인터페이스를 구현한 SmartTV 클래스를 테스트해봅니다.
 		
 		//인터페이스 타입으로 객체를 참조합니다.
-		RemoteControl rc = new SmartTV();
+		RemoteControl rc = new SmartTV();   //workVolume 메소드 테스트
+//		rc.search("");		//오류: 인터페이스 타입 불일치
 		workVolume(rc);		//인터페이스 타입 인자로 메소드 실행합니다.
 		
 //		workSearch(rc);     //오류 : 인자의 타입이 불일치 합니다.
@@ -17,8 +17,13 @@ public class SmartTVTest {
 		//SmartTv 클래스가 2개 인터페이스 구현 클래스이므로 가능합니다.
 		workVolume(new SmartTV());    
 		workSearch(new SmartTV());
+		
+		SmartTV tv = new SmartTV();
+		workVolume(tv);
+		workSearch(tv);
+		workVolume(new Audio());
+//		workSearch(new Audio());  //오류: WebSearch 인터페이스 구현 안함.
 	}
-	
 	//아래의 메소드 2개는 테스트를 위해서 인터페이스 타입 인자를 갖도록 만듭니다.
 	//볼륨을 동작하는 메소드 인자 RemoteControl 타입
 	public static void workVolume(RemoteControl rc) {   //다형성

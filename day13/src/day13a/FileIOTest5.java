45bf94bd7c1a4f6900c14e5fefa8096be8a8cd7b
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-08-05 10:06:41 +0900

    day 14 - 파일복사(day13프로젝트)

--- day13/src/day13a/FileIOTest5.java
+++ day13/src/day13a/FileIOTest5.java
@@ -1,6 +1,7 @@
 package day13a;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 //표준입출력 반복문으로 테스트합니다.
@@ -8,31 +9,50 @@ import java.io.IOException;
 public class FileIOTest5 {
 
 	public static void main(String[] args) {
+		System.out.println("작성자 : 김소희- 실행시간 테스트 합니다.");
 		int b;
 		long start = System.currentTimeMillis();
-		
+		long end;
 		//프로그램에서 입출력 크기를 1바이트 단위에서 임의의 크기(버퍼사용)로 수정해봅시다.
 		byte[] buf = new byte[1024];
-		System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 복사합니다.......");
 		try {   
+			System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 [[바이트단위로]]복사합니다.......");
 			int count=0;
-			FileInputStream fis = new FileInputStream("C:\\Users\\admin\\Desktop\\hoya.jpg");
-			//prob1.txt 파일이 바탕화면에 없으면 FileNotFoundException 발생합니다.
+			FileInputStream fis = new FileInputStream("C:\\Users\\admin\\Desktop\\hoya.jpg");  //파일소스(입력)
+			FileOutputStream fos = new FileOutputStream("C:\\Users\\admin\\Downloads\\hoya.jpg");  //파일출력
+
 			while((b=fis.read())!=-1) {  //입력스트림을 파일로 변경합니다.
 			//while((b=fis.read(buf))!=-1) {   //buf 크기만큼 읽어옵니다. 위의 코드와 실행시간 비교해보세요.
 				count++;  
 			
-				System.out.write(b);   //문자기반의 메소드로 출력
+				fos.write(b);   //문자기반의 메소드로 콘솔 출력
 			//fos.write(buf,0,b);  	
 			}
-			System.out.println(":::" + count + " 바이트 입력! :::");
-			System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 복사 완료!!!.......");	
+			System.out.println(":::" + count + " 바이트 복사되었습니다.! :::");
+			System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 [[바이트단위로]]복사 완료!!!.......");	
 			
+			end = System.currentTimeMillis();
+			System.out.println("실행 시간 : " + (end-start) + " ms 소요되었습니다.\n\n");
+			fos.close(); fis.close(); 		//사용하던 모든 파일을 닫기
+			
+			System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 [[1024바이트 buf 크기단위로]]복사합니다.......");
+			count=0;
+			fis = new FileInputStream("C:\\Users\\admin\\Desktop\\hoya.jpg");    //다시 객체 생성하기
+			fos = new FileOutputStream("C:\\Users\\admin\\Downloads\\hoya3.jpg");
+			start=System.currentTimeMillis();    //새로운 start 시간 저장
+			while((b=fis.read(buf))!= -1) {   
+				//read메소드는 읽어온 바이트 크기를 return 합니다.-마지막 실행때는 1024보다 작거나 같습니다.
+				count+=b;
+				fos.write(buf, 0, b);   //buf 배열의 0번 인덱스부터 읽어온 크기 b 만큼 fos 파일에 출력합니다.
+	 		}
+			System.out.println(":::" + count + " 바이트 복사되었습니다.! :::");
+			System.out.println("바탕화면에 hoya.jpg 파일을 다운로드 경로로 [[1024바이트 buf 크기단위로]]복사 완료!!!.......");	
+			end = System.currentTimeMillis(); //새로운 end 시간 저장
+			System.out.println("실행 시간 : " + (end-start) + " ms 소요되었습니다.\n\n");   //1024 바이트 단위 입출력 시간
+		
+			fos.close();
 			fis.close();    
-		long end = System.currentTimeMillis();
-		System.out.println("실행 시간 : " + (end-start) + " ms 소요되었습니다.");
-			
-		} catch (IOException e) {
+		} catch (IOException e) {  //IOException 의 하위클래스를 모두 처리할수 있습니다.
 			System.out.println("입력 오류 : " + e.getMessage());
 			e.printStackTrace();   //오류내용 추적하여 출력
 		}

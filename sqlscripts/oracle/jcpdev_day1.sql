8875b503b4eb3e117cea32057edeaee93f8980d8
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-08-11 08:14:16 +0900

    sql - day1

---
+++ sqlscripts/oracle/jcpdev_day1.sql
@@ -0,0 +1,11 @@
+-- grant resource,connect to jcpdev
+-- resource 권한이 있으므로 테이블 생성할수 있습니다.
+CREATE TABLE tblTest(
+	col1 varchar2(20),
+	col2 number(6)
+); 
+-- 같은 테이블이름으로 2개를 생성못합니다.
+--(왼쪽탐색기에서 f5 새로고침)
+
+--만들어지 tbltest 테이블의 내용 좀 보자.
+SELECT * FROM TBLTEST;
